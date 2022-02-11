b4d6711bb111b5fd5ea2767b4aa0fa307c16efd6
solarjoni <solarjoni@gmail.com>
2022-02-11 20:42:31 +0200

    Fixture and Test Metadata

---
+++ tests/testMetaScript.js
@@ -0,0 +1,21 @@
+fixture.meta('version', '1')("First ficture")
+    .page("https://www.google.com/")
+    
+    test
+    .meta('env', 'production')
+    .page("https://devexpress.github.io/testcafe/example/")
+    ("First Test", async t => {
+        await t
+        .typeText("#developer-name", "TAU")
+        .click("#macos")
+        .click("#submit-button")
+    })
+    
+    test
+    .page("https://devexpress.github.io/testcafe/example/")
+    ("First Test", async t => {
+        await t
+        .typeText("#developer-name", "TAU")
+        .click("#macos")
+        .click("#submit-button")
+    })
