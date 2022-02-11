3d0d95825362ecd358fc6dfd0440d8bbc28fc958
solarjoni <solarjoni@gmail.com>
2022-02-11 20:29:47 +0200

    Specify Web Page

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,7 +1,6 @@
 fixture("First ficture")
-    .page("https://www.google.com/")
-    test.page("https://devexpress.github.io/testcafe/example/")
-    ("First Test", async t => {
+    .page("https://devexpress.github.io/testcafe/example/")
+    test("First Test", async t => {
         await t
         .typeText("#developer-name", "TAU")
         .click("#macos")
