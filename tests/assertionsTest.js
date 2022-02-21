ac7b7d7819349e70720d2a8bc40eb37ab98d02f9
solarjoni <solarjoni@gmail.com>
2022-02-21 22:08:16 +0200

    Wait Mechanism

--- tests/assertionsTest.js
+++ tests/assertionsTest.js
@@ -8,11 +8,14 @@ fixture("First ficture")
     .page("https://devexpress.github.io/testcafe/example/")
 
 test("First Test", async t => {
+    const developerNameElement = 
+    await developerName.with({visibilityCheck: true})()
     await t
     .setTestSpeed(0.1)
-    .expect(developerName.value).eql("", "input is empty")
+    .expect(developerNameElement.value).eql("", "input is empty")
     .typeText(developerName, "TAU")
     .expect(developerName.value).eql("TAU", "input equals 'TAU' ")
+    .takeElementScreenshot(developerName)
     .click(osOption)
     .click(submitButton)
 })
